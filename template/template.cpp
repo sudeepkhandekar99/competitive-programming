ac931f577a127d4a1a9bff02be25bdb8c65de06d
sudeepkhandekar99 <sudeepkhandekar99@gmail.com>
2021-06-04 22:14:34 +0530

    template

---
+++ template/template.cpp
@@ -0,0 +1,27 @@
+// Author: Sudeep Khandekar
+// Email: sudeepkhandekar99@gmail.com
+// g++ upload.cpp && ./a.out < in.txt > out.txt
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int128 __int128_t
+#define ddl long double
+#define ll long long
+#define dd double
+#define endl "\n"
+#define tab "\t"
+#define vector<int> vi;
+#define pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i < b; i++)
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
